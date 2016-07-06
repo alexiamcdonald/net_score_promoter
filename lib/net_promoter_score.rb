503d21306430426e2070a765d81410d72fad8d88
Alexia McDonald <alexiamcdonald@donesafe.com>
2016-07-06 22:39:13 +1000

    Refactor everything

--- lib/net_promoter_score.rb
+++ lib/net_promoter_score.rb
@@ -13,11 +13,7 @@ def red_data(data)
   red_total = red_data.size.to_f
   red_total/data_total(data) * 100
 end
-#
-# def nps(score)
-#   green_data = collect_green(score)
-#   green_data = green_score(green_data, score)
-#   red_data = collect_red(score)
-#   red_data = red_score(red_data, score)
-#   green_data - red_data
-# end
+
+def net_score(data)
+  green_data(data) - red_data(data)
+end
--- spec/net_promoter_score_spec.rb
+++ spec/net_promoter_score_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe 'net_promoter_score' do
 
     subject { green_data(data)}
 
-    it 'collects all the green data' do
+    it 'prints the percentage for green data' do
       expect(subject).to eq(40)
     end
   end
@@ -17,16 +17,16 @@ RSpec.describe 'net_promoter_score' do
 
     subject { red_data(data) }
 
-    it 'prints the percentage for reds' do
+    it 'prints the percentage for red data' do
       expect(subject).to eq(50)
     end
   end
 
-#   describe '#nps' do
-#     subject {nps(score)}
-#
-#     it 'gives the score for the nps' do
-#       expect(subject).to eq(-10)
-#     end
-#   end
+  describe '#net_score' do
+    subject { net_score(data) }
+
+    it 'gives the score for the net promoter score' do
+      expect(subject).to eq(-10)
+    end
+  end
 end
