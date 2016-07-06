27c3d5f8fe3d875649e8e66c626a39d0b29552fb
Alexia McDonald <alexiamcdonald@donesafe.com>
2016-07-06 22:28:55 +1000

    Refactor green

--- lib/net_promoter_score.rb
+++ lib/net_promoter_score.rb
@@ -1,27 +1,27 @@
-def collect_green(score)
-  score.select {|num| num >= 9}
+def data_total(data)
+  data.size.to_f
 end
 
-def green_score(green_data, data)
-  score = green_data.size.to_f
-  total = data.size.to_f
-  score/total * 100
+def green_data(data)
+  green_data = data.select {|num| num >= 9}
+  green_total = green_data.size.to_f
+  green_total/data_total(data) * 100
 end
-
-def collect_red(score)
-  score.select {|num| num < 7}
-end
-
-def red_score(red_data, data)
-  score = red_data.size.to_f
-  total = data.size.to_f
-  score/total * 100
-end
-
-def nps(score)
-  green_data = collect_green(score)
-  green_data = green_score(green_data, score)
-  red_data = collect_red(score)
-  red_data = red_score(red_data, score)
-  green_data - red_data
-end
+#
+# def collect_red(score)
+#   score.select {|num| num < 7}
+# end
+#
+# def red_score(red_data, data)
+#   score = red_data.size.to_f
+#   total = data.size.to_f
+#   score/total * 100
+# end
+#
+# def nps(score)
+#   green_data = collect_green(score)
+#   green_data = green_score(green_data, score)
+#   red_data = collect_red(score)
+#   red_data = red_score(red_data, score)
+#   green_data - red_data
+# end
